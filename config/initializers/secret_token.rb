e4fbf6a7d64a7a41b87387f68494f1b565df95f4
asonas <hzw1258@gmail.com>
2013-01-24 22:50:36 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FlickrClone::Application.config.secret_token = 'a9038bcee0c049075167451292f84de90edcac9bfabf141e0cffa9b027d780a916c936f72d6274ab9f4d1a8bc103b1c760ff94a8433989562068858d6497b038'
